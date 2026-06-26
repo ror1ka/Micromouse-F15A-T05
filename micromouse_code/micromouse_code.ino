cecd14e0671f022ba3c89ced8e3794ad62eb3653
Benjamin Jiang <z5589775@ad.unsw.edu.au>
2026-06-26 15:29:20 +1000

    attempted to use IMU

--- micromouse_code/micromouse_code.ino
+++ micromouse_code/micromouse_code.ino
@@ -4,20 +4,36 @@
 #include "Encoder.hpp"
 #include "BangBangController.hpp"
 #include "Micromouse.hpp"
+#include "Wire.h"
 
 #define BAUD 9600
 
 PIDController posPID(1.5, 1.0, 0.0);
 Micromouse mouse(posPID);
+unsigned long timer = 0;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(BAUD);
+  Wire.begin();
+
+  // mouse.setupIMU();
 
   delay(1000);
 }
 
 void loop() {
+  //   mouse.getIMU().update();
+  
+  // if((millis()-timer)>10){ // print data every 10ms
+	// Serial.print("X : ");
+	// Serial.print(mouse.getIMU().getAngleX());
+	// Serial.print("\tY : ");
+	// Serial.print(mouse.getIMU().getAngleY());
+	// Serial.print("\tZ : ");
+	// Serial.println(mouse.getIMU().getAngleZ());
+	// timer = millis();
+  // }
   // Serial.println(String("ENC1: ") + mouse.getEncoder(2).getRotation());
   mouse.travelDistance(200, 50);
 
@@ -39,11 +55,10 @@ void loop() {
   delay(1000);
   mouse.turnRight(50);
   delay(1000);
+  
+  while(true) {};
 
 
-  // delay(1000);
-
-  // // put your main code here, to run repeatedly:
   // encoder1.readEncoder();
   // Serial.print("ENC1: ") + Serial.println(encoder1.count);
   // Serial.print("ENC2: ") + Serial.println(encoder2.count);
