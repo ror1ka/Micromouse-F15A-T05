0db674019cd4aed2fe57597814edbd686d5dd8f2
Benjamin Jiang <z5589775@ad.unsw.edu.au>
2026-07-05 23:55:01 +1000

    Updated Turning Code to turn with IMU

--- micromouse_code/Micromouse.hpp
+++ micromouse_code/Micromouse.hpp
@@ -9,10 +9,11 @@
 #include "Wire.h"
 #include <MPU6050_light.h>
 
+MPU6050 mpu(Wire);
+
 #define LEFT 1
 #define RIGHT 2
-#define WHEEL_TURN 4.873
-#define WHEEL_TURN_ERR 0.25
+#define ROT_ERR 20
 #define DIST_ERR 5
 #define Dist_OFFSET 1
 
@@ -20,136 +21,152 @@
 
 class Micromouse {
 public:
-    Micromouse(PIDController pid) : pid(pid) {}
+  Micromouse(PIDController pid)
+    : pid(pid) {}
 
-    // Helper Funct
-    void turnMotorLeft(int16_t speed) {
-      leftMotor.setPWM(speed);
-      rightMotor.setPWM(speed);
-    }
+  // Helper Funct
+  void turnMotorLeft(int16_t speed) {
+    leftMotor.setPWM(speed);
+    rightMotor.setPWM(speed);
+  }
 
-    void turnMotorRight(int16_t speed) {
-      leftMotor.setPWM(-speed);
-      rightMotor.setPWM(-speed);
-    }
+  void turnMotorRight(int16_t speed) {
+    leftMotor.setPWM(-speed);
+    rightMotor.setPWM(-speed);
+  }
 
-    void move(int16_t speed) {
-      leftMotor.setPWM(-speed);
-      rightMotor.setPWM(speed);
-    }
+  void move(int16_t speed) {
+    leftMotor.setPWM(-speed);
+    rightMotor.setPWM(speed);
+  }
 
-    // void setupIMU() {
-    //   byte status = getIMU().begin();
-    //   Serial.print(F("MPU6050 status: "));
-    //   Serial.println(status);
-    //   while(status!=0){ } // stop everything if could not connect to MPU6050
+  void setupIMU() {
+    byte status = mpu.begin();
+    Serial.print(F("MPU6050 status: "));
+    Serial.println(status);
+    while (status != 0) {}  // stop everything if could not connect to MPU6050
 
-    //   Serial.println(F("Calculating offsets, do not move MPU6050"));
-    //   delay(1000);
-    //   // mpu.upsideDownMounting = true; // uncomment this line if the MPU6050 is mounted upside-down
-    //   getIMU().calcOffsets(); // gyro and accelero
-    //   Serial.println("Done!\n");
-    // }
+    Serial.println(F("Calculating offsets, do not move MPU6050"));
+    delay(1000);
+    // mpu.upsideDownMounting = true; // uncomment this line if the MPU6050 is mounted upside-down
+    mpu.calcOffsets();  // gyro and accelero
+    Serial.println("Done!\n");
+  }
 
-    // Main Func
-    void turnLeft(int16_t speed) {
-      resetEnc();
-      pid.zeroAndSetTarget(leftEncoder.getRotation(), -WHEEL_TURN);
-      pid.compute(leftEncoder.getRotation());
-      while(pid.getError() < 0) {
-          if (pid.getError() < -WHEEL_TURN_ERR) {
-            turnMotorLeft(speed);
-          } else {
-            turnMotorLeft(speed * abs(pid.getError())/WHEEL_TURN_ERR + 10);
-          }
-          // Serial.println(String("Err: ") + pid.getError());
-          pid.compute(leftEncoder.getRotation());
-      }
+  // Main Func
+  void turnLeft(int16_t speed) {
+    const float target = getRot() + 90;
+    const float err = target - ROT_ERR;
+    while (getRot() < target) {
+      mpu.update();
 
-      turnMotorLeft(0);
-    }
+      // Debugging
+      printIMU();
 
-    void turnRight(int16_t speed) {
-        resetEnc();
-      pid.zeroAndSetTarget(leftEncoder.getRotation(), WHEEL_TURN);
-      pid.compute(leftEncoder.getRotation());
-      while(pid.getError() > 0) {
-          if (pid.getError() > WHEEL_TURN_ERR) {
-            turnMotorRight(speed);
-          } else {
-            turnMotorRight(speed * pid.getError()/WHEEL_TURN_ERR + 10);
-          }
-          // Serial.println(String("Err: ") + pid.getError());
-          pid.compute(leftEncoder.getRotation());
-      }
+      // Serial.print("\tSpeed : ");
+      // Serial.println(speed * abs(target - getRot()) / ROT_ERR);
 
-      turnMotorRight(0);
-    }
-
-    // dist in mm
-    void travelDistance(uint16_t dist, int16_t speed) {
-        resetEnc();
-        pid.zeroAndSetTarget(getCurrDist(), dist - Dist_OFFSET);
-        pid.compute(getCurrDist());
-        Serial.println(String("Dist: ") + getCurrDist());
-        while(pid.getError() > 0) {
-          if (pid.getError() > DIST_ERR) {
-            move(speed);
-          } else {
-            move(speed * pid.getError()/DIST_ERR + 5);
-          }
-          Serial.println(String("Err: ") + pid.getError());
-          pid.compute(getCurrDist());
-        }
-
-        move(0);
-    }
-
-
-    // Get Func for debugging purposes
-    Encoder getEncoder(int num) {
-      if (num == LEFT) {
-        return leftEncoder;
+      if (getRot() > err) {
+        turnMotorLeft(speed * abs(target - getRot()) / ROT_ERR + 20);
       } else {
-        return rightEncoder;
+        turnMotorLeft(speed);
       }
     }
 
-    // MPU6050 getIMU() {
-    //   return mpu;
-    // }
+    turnMotorLeft(0);
+  }
 
-    Motor getLeftMotor() {
-      return leftMotor;
+  void turnRight(int16_t speed) {
+    float target = getRot() - 90;
+    const float err = target + ROT_ERR;
+    while (getRot() > target) {
+      mpu.update();
+
+      // Debugging
+      printIMU();
+
+      // Serial.print("\tSpeed : ");
+      // Serial.println(speed * abs(target - getRot()) / ROT_ERR);
+
+      if (getRot() < err) {
+        turnMotorRight((speed * abs(target - getRot()) / ROT_ERR) + 20);
+      } else {
+        turnMotorRight(speed);
+      }
     }
 
-    Motor getRightMotor() {
-      return rightMotor;
+    turnMotorRight(0);
+  }
+
+  // dist in mm
+  void travelDistance(uint16_t dist, int16_t speed) {
+    resetEnc();
+    pid.zeroAndSetTarget(getCurrDist(), dist - Dist_OFFSET);
+    pid.compute(getCurrDist());
+    Serial.println(String("Dist: ") + getCurrDist());
+    while (pid.getError() > 0) {
+      // if (pid.getError() > DIST_ERR) {
+      move(speed);
+      // } else {
+      //   move(speed * pid.getError()/DIST_ERR + 5);
+      // }
+      // Serial.println(String("Err: ") + pid.getError());
+      pid.compute(getCurrDist());
     }
 
-    float getCurrDist() {
-      return abs(leftEncoder.getRotation()/(2*PI)) * getWheelCir();
+    move(0);
+  }
+
+
+  // Get Func for debugging purposes
+  Encoder getEncoder(int num) {
+    if (num == LEFT) {
+      return leftEncoder;
+    } else {
+      return rightEncoder;
     }
+  }
+
+  Motor getLeftMotor() {
+    return leftMotor;
+  }
+
+  Motor getRightMotor() {
+    return rightMotor;
+  }
+
+  float getCurrDist() {
+    return abs(leftEncoder.getRotation() / (2 * PI)) * getWheelCir();
+  }
+
+  void printIMU() {
+    Serial.print("\tZ : ");
+    Serial.println(mpu.getAngleZ());
+  }
+
+  float getRot() {
+    return mpu.getAngleZ();
+  }
 
 private:
-    Motor leftMotor = Motor(MOT1PWM,MOT1DIR);
-    Motor rightMotor = Motor(MOT2PWM,MOT2DIR);
-    Encoder leftEncoder = Encoder(EN1_A, EN1_B, LEFT);
-    Encoder rightEncoder = Encoder(EN2_A, EN2_B, RIGHT);
-    PIDController pid;
-    // MPU6050 mpu(Wire);
+  Motor leftMotor = Motor(MOT1PWM, MOT1DIR);
+  Motor rightMotor = Motor(MOT2PWM, MOT2DIR);
+  Encoder leftEncoder = Encoder(EN1_A, EN1_B, LEFT);
+  Encoder rightEncoder = Encoder(EN2_A, EN2_B, RIGHT);
+  PIDController pid;
+  // MPU6050 mpu(Wire);
 
-    void resetEnc() {
-      leftEncoder.resetCount();
-      rightEncoder.resetCount();
-    }
+  void resetEnc() {
+    leftEncoder.resetCount();
+    rightEncoder.resetCount();
+  }
 
-    float getWheelCir() {
-      return wheelDiameter * PI;
-    }
+  float getWheelCir() {
+    return wheelDiameter * PI;
+  }
 
-    // In mm
-    int wheelDiameter = 32;
-    int rotCount = 500;
-    uint16_t counts_per_revolution = 1400;
+  // In mm
+  int wheelDiameter = 32;
+  int rotCount = 500;
+  uint16_t counts_per_revolution = 1400;
 };
--- micromouse_code/micromouse_code.ino
+++ micromouse_code/micromouse_code.ino
@@ -6,9 +6,9 @@
 #include "Micromouse.hpp"
 #include "Wire.h"
 
-#define BAUD 9600
+#define BAUD 115200
 
-PIDController posPID(1.5, 1.0, 0.0);
+PIDController posPID(2.0, 1.0, 2.0);
 Micromouse mouse(posPID);
 unsigned long timer = 0;
 
@@ -17,84 +17,38 @@ void setup() {
   Serial.begin(BAUD);
   Wire.begin();
 
-  // mouse.setupIMU();
+  mouse.setupIMU();
 
   delay(1000);
 }
 
 void loop() {
-  //   mouse.getIMU().update();
+  // mouse.travelDistance(200, 100);
+
+  // delay(1000);
+
+  mouse.turnLeft(100);
+  delay(1000);
+  mouse.turnRight(100);
+  delay(1000);
+  // mouse.turnLeft(100);
+  // delay(1000);
+  // mouse.turnLeft(100);
+  // delay(1000);
+  // mouse.turnLeft(100);
+  // delay(1000);
+  // mouse.turnRight(100);
+  // delay(1000);
+  // mouse.turnRight(100);
+  // delay(1000);
+  // mouse.turnRight(100);
+  // delay(1000);
+  // mouse.turnRight(100);
+  // delay(1000);
   
-  // if((millis()-timer)>10){ // print data every 10ms
-	// Serial.print("X : ");
-	// Serial.print(mouse.getIMU().getAngleX());
-	// Serial.print("\tY : ");
-	// Serial.print(mouse.getIMU().getAngleY());
-	// Serial.print("\tZ : ");
-	// Serial.println(mouse.getIMU().getAngleZ());
-	// timer = millis();
-  // }
-  // Serial.println(String("ENC1: ") + mouse.getEncoder(2).getRotation());
-  mouse.travelDistance(200, 50);
-
-  delay(1000);
-
-  mouse.turnLeft(50);
-  delay(1000);
-  mouse.turnLeft(50);
-  delay(1000);
-  mouse.turnLeft(50);
-  delay(1000);
-  mouse.turnLeft(50);
-  delay(1000);
-  mouse.turnRight(50);
-  delay(1000);
-  mouse.turnRight(50);
-  delay(1000);
-  mouse.turnRight(50);
-  delay(1000);
-  mouse.turnRight(50);
-  delay(1000);
-  
-  while(true) {};
-
+  // while(true) {};
 
   // encoder1.readEncoder();
   // Serial.print("ENC1: ") + Serial.println(encoder1.count);
   // Serial.print("ENC2: ") + Serial.println(encoder2.count);
-  // // Move forward
-  // motor1.setPWM(50);
-  // motor2.setPWM(-50);
-
-  // delay(2000);
-
-  // // Stop
-  // motor1.setPWM(0);
-  // motor2.setPWM(0);
-
-  // delay(2000);
-
-  // // Turn arround
-  // motor1.setPWM(50);
-  // motor2.setPWM(50);
-
-  // delay(1500);
-
-  //   // Stop
-  // motor1.setPWM(0);
-  // motor2.setPWM(0);
-
-  // delay(2000);
-
-  // // Record Turns
-  // Serial.print("ENC1: ") + Serial.println(encoder1.count);
-  //   // Turn arround
-
-  // delay(1600);
-
-  //   // Stop
-  // motor1.setPWM(0);
-  // motor2.setPWM(0);
-
-  // delay(2000);
 }
