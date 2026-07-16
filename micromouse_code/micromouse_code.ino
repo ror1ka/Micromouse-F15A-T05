be8bbc4f32ad7567de85638296e8256a5c577301
Benjamin Jiang <z5589775@ad.unsw.edu.au>
2026-07-16 11:50:45 +1000

    coded task 3 Chaining for micromouse

--- micromouse_code/micromouse_code.ino
+++ micromouse_code/micromouse_code.ino
@@ -23,32 +23,7 @@ void setup() {
 }
 
 void loop() {
-  // mouse.travelDistance(200, 100);
-
-  // delay(1000);
-
-  mouse.turnLeft(100);
-  delay(1000);
-  mouse.turnRight(100);
-  delay(1000);
-  // mouse.turnLeft(100);
-  // delay(1000);
-  // mouse.turnLeft(100);
-  // delay(1000);
-  // mouse.turnLeft(100);
-  // delay(1000);
-  // mouse.turnRight(100);
-  // delay(1000);
-  // mouse.turnRight(100);
-  // delay(1000);
-  // mouse.turnRight(100);
-  // delay(1000);
-  // mouse.turnRight(100);
-  // delay(1000);
-  
-  // while(true) {};
-
-  // encoder1.readEncoder();
-  // Serial.print("ENC1: ") + Serial.println(encoder1.count);
-  // Serial.print("ENC2: ") + Serial.println(encoder2.count);
+  // Task 3 Chaining
+  char *s = "lffrflfr"
+  mouse.chainMovement();
 }
