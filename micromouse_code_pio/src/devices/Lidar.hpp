ed4193cab23059752b4798d54c6979bdbae81adb
Benjamin Jiang <z5589775@ad.unsw.edu.au>
2026-08-07 17:57:18 +1000

    Added setup code for lidar

--- micromouse_code_pio/src/devices/Lidar.hpp
+++ micromouse_code_pio/src/devices/Lidar.hpp
@@ -2,10 +2,37 @@
 
 #include <Arduino.h>
 #include "Wire.h"
+#include "VL6180X.h"
 
 class Lidar {
 public:
+    Lidar(VL6180X s, uint8_t pin, uint8_t address) : s(s), pin(pin), address(address) {
+        pinMode(pin, OUTPUT);
+        digitalWrite(pin, LOW);
+
+        delay(50);
+
+        digitalWrite(pin, HIGH);
+
+        s.init();
+        s.configureDefault();
+        s.writeReg(VL6180X::SYSRANGE__MAX_CONVERGENCE_TIME, 0x14);
+        s.setTimeout(100);
+        s.setAddress(address);
+
+        Serial.print("LiDAR on pin ");
+        Serial.print(pin);
+        Serial.print(" assigned address 0x");
+        Serial.println(address, HEX);
+
+        s.setTimeout(1000);
+
+        s.readRangeSingleMillimeters();
+    }
+
 
 private:
-
+    VL6180X s;
+    uint8_t pin;
+    uint8_t address;
 };
