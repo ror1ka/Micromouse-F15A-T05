51e26514b0e6dfe8ea21332bdec0ac4458b70424
Benjamin Jiang <z5589775@ad.unsw.edu.au>
2026-08-20 18:41:48 +1000

    Tested and fully completes 4.2

--- micromouse_code_pio/src/task4_code/4_2_full_route.h
+++ micromouse_code_pio/src/task4_code/4_2_full_route.h
@@ -7,10 +7,15 @@ struct Command {
     float value;
 };
 
-constexpr int NUM_COMMANDS = 42;
+constexpr int NUM_COMMANDS = 47;
 constexpr Command MISSION[NUM_COMMANDS] = {
     {'r', 0.0},
     {'f', 0.0},
+    {'f', 0.0},
+    {'l', 0.0},
+    {'f', 0.0},
+    {'r', 0.0},
+    {'f', 0.0},
     {'l', 0.0},
     {'f', 0.0},
     {'l', 0.0},
@@ -26,8 +31,8 @@ constexpr Command MISSION[NUM_COMMANDS] = {
     {'f', 0.0},
     {'r', 0.0},
     {'f', 0.0},
-    {'l', 0.0},
-    {'f', 0.0},
+    {'T', 90.0},
+    {'D', 180.0},
     {'T', 54.7},
     {'D', 401.5},
     {'T', -41.9},
